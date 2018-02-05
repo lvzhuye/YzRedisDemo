8103f092f0c67f1548c4a05d76d839148d330c12
yangzhi <yangzhi2@outlook.com>
2018-02-05 20:44:29 +0800

    listdemo

--- ChapterOne/Services/ArticleService.cs
+++ ChapterOne/Services/ArticleService.cs
@@ -40,17 +40,67 @@ namespace ChapterOne.Services
             //string val4 = redis.StringGet("testkey");
 
             var listkey = "listkey";
-            redis.KeyDelete(listkey,CommandFlags.FireAndForget);
+            var result = redis.KeyDelete(listkey, CommandFlags.FireAndForget);
 
-            var result = redis.ListRightPush(listkey,"a");
+            long result2 = redis.ListRightPush(listkey,"a");
 
             var len = redis.ListLength(listkey);
 
-            result = redis.ListRightPush(listkey,"b");
+            result2 = redis.ListRightPush(listkey,"b");
 
             len = redis.ListLength(listkey);
 
-            
+            result = redis.KeyDelete(listkey);
+
+            result2 = redis.ListRightPush(listkey, "abcdefghijklmnopqrstuvwxyz".Select(x => (RedisValue)x.ToString()).ToArray());
+
+            len = redis.ListLength(listkey);
+
+            var result3 = string.Concat(redis.ListRange(listkey));
+
+            var lastFive = redis.ListRange(listkey,-5);
+
+            result3 = string.Concat(lastFive);
+
+            var firstFive = redis.ListRange(listkey,0,4);
+
+            redis.ListTrim(listkey,0,1);
+
+            var trimedList = redis.ListRange(listkey);
+
+            result = redis.KeyDelete(listkey,CommandFlags.FireAndForget);
+
+            result2 = redis.ListRightPush(listkey, "abcdefghijklmnopqrstuvwxyz".Select(x => (RedisValue)x.ToString()).ToArray());
+
+            var firstElement = redis.ListLeftPop(listkey);
+
+            var lastElement = redis.ListRightPop(listkey);
+
+            long result4 = redis.ListRemove(listkey,"c");
+
+            var removedList = string.Concat(redis.ListRange(listkey));
+
+            //???
+            redis.ListSetByIndex(listkey, 1, "c");
+
+            var listSetByIndexed = string.Concat(redis.ListRange(listkey));
+
+            var thirdItem = redis.ListGetByIndex(listkey,3);
+
+            var destinationKey = "destinationList";
+            result = redis.KeyDelete(listkey,CommandFlags.FireAndForget);
+            result = redis.KeyDelete(listkey,CommandFlags.FireAndForget);
+
+            result2 = redis.ListRightPush(listkey, "abcdefghijklmnopqrstuvwxyz".Select(x => (RedisValue)x.ToString()).ToArray());
+
+            len = redis.ListLength(listkey);
+
+            for (var i = 0; i < len; i++)
+            {
+                string val = redis.ListRightPopLeftPush(listkey, destinationKey);
+            }
+
+
         }
     }
 }
